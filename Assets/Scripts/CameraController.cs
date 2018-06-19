fdfa8211f3034294df1b32a0c135608670e5a5a1
BeneDev <33699887+BeneDev@users.noreply.github.com>
2018-06-19 12:57:28 +0200

    Basic Player and Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    PlayerController player;
+    [SerializeField] float lerpSpeed = 1f;
+
+	// Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, lerpSpeed);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The GameManager script, which controls several aspects of the game unaffected by loading scenes etc.
+/// </summary>
+public class GameManager : Singleton<GameManager> {
+
+    #region Fields
+
+    #endregion
+
+    #region Unity Messages
+    
+    #endregion
+
+    #region Helper Methods
+    
+    #endregion
+
+}
