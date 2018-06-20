9c064a0ac9177c8fd086b2b5ec2d61bab99c9e81
TastyCooler <34125083+TastyCooler@users.noreply.github.com>
2018-06-20 22:44:21 +0200

    Prototype dash+ trail + combo feature

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    PlayerController pC;
+    
+
+    private void Awake()
+    {
+        pC = GetComponent<PlayerController>();
+        pC = FindObjectOfType<PlayerController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        pC.combo++;
+    }
+  
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,17 +38,31 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] GameObject cursor;
 
+    
+    private Rigidbody2D rb;
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private Vector3 target;
+
+    public int combo;
+    public float comboTime = 50f;
+
     public enum State
     {
         freeToMove,
         dashing,
         attacking
     }
-    State playerState = State.freeToMove;
+    State playerState = State.dashing;
 
     private void Awake()
     {
         input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
+        target = transform.position;
+
     }
 
     private void Update()
@@ -73,14 +87,77 @@ public class PlayerController : MonoBehaviour {
         else if(playerState == State.dashing)
         {
             print("I'm dashing");
+            if (Input.GetMouseButtonDown(0))
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                target.z = transform.position.z;
+            }
+            if(dashTime <= 0)
+            {
+                dashTime = startDashTime;
+                rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                
+                dashTime -= Time.deltaTime;
+            }
+
+
+            transform.position = Vector3.MoveTowards(transform.position, target, dashSpeed);
+            Combo();
         }
         cursor.transform.position = transform.position + moveDirection;
         Debug.LogFormat("X: {0} | Y: {1} | Z: {2}", moveDirection.x, moveDirection.y, moveDirection.z);
         //cursor.transform.localRotation.SetFromToRotation(cursor.transform.up, moveDirection);
         //cursor.transform.rotation.SetEulerAngles(cursor.transform.rotation.x, cursor.transform.rotation.y, );
         cursor.transform.rotation.SetLookRotation(moveDirection);
+
+       
     }
 
+    void Combo()
+    {
+        if (combo >= 1)
+        {
+            //Debug.Log(comboTime);
+            Debug.Log(combo);
+            comboTime -= Time.deltaTime;
+            if (comboTime == 0)
+            {
+                comboTime = 0;
+                combo -= 1;
+            }
+        }
+
+        if (combo >= 3)
+        {
+            Special();
+        }
+
+    }
+
+    void Special()
+    {
+        float distanceToClosestEnemy = Mathf.Infinity;
+        Enemy closestEnemy = null;
+        Enemy[] allEnemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy currentEnemy in allEnemies)
+        {
+            float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceToClosestEnemy)
+            {
+                distanceToClosestEnemy = distanceToEnemy;
+                closestEnemy = currentEnemy;
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, closestEnemy.transform.position, dashSpeed * speed);
+        }
+        Debug.DrawLine(transform.position, closestEnemy.transform.position);
+    }
     void GetInput()
     {
         moveDirection.x = input.Horizontal;
