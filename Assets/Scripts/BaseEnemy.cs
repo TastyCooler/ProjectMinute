1825fc0820fc738dd88a7900cac6676cc6bd3102
BeneDev <33699887+BeneDev@users.noreply.github.com>
2018-06-21 11:44:52 +0200

    Enemy cant see through walls

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -37,6 +37,57 @@ public class BaseEnemy : MonoBehaviour {
         toPlayer = player.transform.position - transform.position;
     }
 
+    protected virtual void Patrolling()
+    {
+        if (V3Equal(targetPos, Vector3.zero, 0.1f))
+        {
+            DefineNewTargetPos();
+        }
+        if (toPlayer.magnitude < sightReach)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, toPlayer.normalized, sightReach + 10f);
+            if(hit.collider)
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    enemyState = State.playerSpotted;
+                }
+            }
+        }
+        if (!V3Equal(transform.position, targetPos, 0.1f))
+        {
+            transform.position += (targetPos - transform.position).normalized * (speed * patrollingSpeedMultiplier) * Time.deltaTime;
+        }
+        else
+        {
+            DefineNewTargetPos();
+        }
+    }
+
+    protected virtual void PursuitPlayer()
+    {
+        transform.position += toPlayer.normalized * speed * Time.deltaTime;
+        if (toPlayer.magnitude > sightReach)
+        {
+            enemyState = State.searchingForPlayer;
+            playerLastSpottedAt = player.transform.position;
+        }
+    }
+
+    protected virtual void SearchForPlayer()
+    {
+        if (toPlayer.magnitude < sightReach)
+        {
+            enemyState = State.playerSpotted;
+        }
+        transform.position += (playerLastSpottedAt - transform.position).normalized * speed * Time.deltaTime;
+        if (V3Equal(transform.position, playerLastSpottedAt, 0.1f))
+        {
+            enemyState = State.patrolling;
+            DefineNewTargetPos();
+        }
+    }
+
 	public void TakeDamage(int damage, Vector3 knockback)
     {
         health -= damage;
@@ -44,5 +95,17 @@ public class BaseEnemy : MonoBehaviour {
         {
             // TODO kill the enemy
         }
+    }
+
+    protected void DefineNewTargetPos()
+    {
+        Vector2 relativePoint = Random.insideUnitCircle * patrolRadius;
+        targetPos = transform.position + new Vector3(relativePoint.x, relativePoint.y, 0f);
+    }
+
+    // Check if two vectors are the same taking into account a small margin, in this case, the aimingtolerance
+    protected bool V3Equal(Vector3 a, Vector3 b, float aimingTolerance)
+    {
+        return Vector3.SqrMagnitude(a - b) < aimingTolerance;
     }
 }
--- Assets/Scripts/CrawlerEnemy.cs
+++ Assets/Scripts/CrawlerEnemy.cs
@@ -13,60 +13,17 @@ public class CrawlerEnemy : BaseEnemy {
     protected override void Update()
     {
         base.Update();
-        if(enemyState == State.patrolling)
+        if (enemyState == State.patrolling)
         {
-            if(V3Equal(targetPos, Vector3.zero, 0.1f))
-            {
-                DefineNewTargetPos();
-            }
-            if(toPlayer.magnitude < sightReach)
-            {
-                enemyState = State.playerSpotted;
-            }
-            if(!V3Equal(transform.position, targetPos, 0.1f))
-            {
-                transform.position += (targetPos - transform.position).normalized * (speed * patrollingSpeedMultiplier) * Time.deltaTime;
-            }
-            else
-            {
-                DefineNewTargetPos();
-            }
+            Patrolling();
         }
-        else if(enemyState == State.playerSpotted)
+        else if (enemyState == State.playerSpotted)
         {
-            transform.position += toPlayer.normalized * speed * Time.deltaTime;
-            if(toPlayer.magnitude > sightReach)
-            {
-                enemyState = State.searchingForPlayer;
-                playerLastSpottedAt = player.transform.position;
-            }
+            PursuitPlayer();
         }
-        else if(enemyState == State.searchingForPlayer)
+        else if (enemyState == State.searchingForPlayer)
         {
-            if(toPlayer.magnitude < sightReach)
-            {
-                enemyState = State.playerSpotted;
-            }
-            transform.position += (playerLastSpottedAt - transform.position).normalized * speed * Time.deltaTime;
-            if(V3Equal(transform.position, playerLastSpottedAt, 0.1f))
-            {
-                enemyState = State.patrolling;
-                DefineNewTargetPos();
-            }
-
+            SearchForPlayer();
         }
-    }
-
-    void DefineNewTargetPos()
-    {
-        Vector2 relativePoint = Random.insideUnitCircle * patrolRadius;
-        targetPos = transform.position + new Vector3(relativePoint.x, relativePoint.y, 0f);
-
-    }
-
-    // Check if two vectors are the same taking into account a small margin, in this case, the aimingtolerance
-    bool V3Equal(Vector3 a, Vector3 b, float aimingTolerance)
-    {
-        return Vector3.SqrMagnitude(a - b) < aimingTolerance;
     }
 }
