32ec6bb6e557f817ff06182f87508cd2ec0dfc98
Bene_Dev <seriousbenedikt@web.de>
2018-07-09 21:09:44 +0200

    Camera goes around for title mode

---
+++ Assets/Scripts/TitlescreenController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitlescreenController : MonoBehaviour {
+
+    Animator anim;
+
+    public event System.Action OnGameStarted;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnPlayButton()
+    {
+        anim.SetTrigger("Hide");
+        if(OnGameStarted != null)
+        {
+            OnGameStarted();
+        }
+    }
+}
