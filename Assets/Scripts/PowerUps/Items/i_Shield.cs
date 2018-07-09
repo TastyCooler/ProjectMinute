79320fe40fbd717173623dea5a4a3fe33c3ce5bd
Eddie <>
2018-07-09 15:08:22 +0200

    Some Work On the Shield
    literaly no progress yet.
    About to add a new beziercurve:
    https://catlikecoding.com/unity/tutorials/curves-and-splines/

---
+++ Assets/Scripts/PowerUps/Items/i_Shield.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class i_Shield : BaseItem {
+
+    [SerializeField] int usages = 5;
+    [SerializeField] GameObject Shield;
+
+    public override void Use()
+    {
+        if (usageTimes <= usages)
+        {
+            usages--;
+            Debug.Log("Shield used");
+            ActivateShield();
+        }
+    }
+
+    void ActivateShield()
+    {
+        GameObject shield = Instantiate(Shield, player.transform);
+        
+    }
+}
