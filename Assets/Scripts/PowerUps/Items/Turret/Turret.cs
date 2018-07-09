c6f32461e88d0b602dc4e133b8c07613cdb12d4c
Eddie <>
2018-07-09 10:27:33 +0200

    Implemented Turret item
    - Tower doesnt take damage yet.
    - Doesn't deal damage.
    - Not sure if it works 100%
    
    + Shoots in enemy direction
    + Got a lifetime

---
+++ Assets/Scripts/PowerUps/Items/Turret/Turret.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    PlayerController player;
+
+    [SerializeField] int timer; // The Shootrate
+    const int initialtimer = 30; // Used to reset timer
+
+    [SerializeField] int lifetime = 100; // Towers lifetime, get decreased per shot
+
+    [SerializeField] float sightreach = 7f; // towers sight reach 
+    Vector3 enemypos;
+    Vector3 toEnemy; // targets enemy
+
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        timer = initialtimer;
+       
+    }
+
+    private void Update()
+    {
+        if(lifetime >= 0)
+        {
+            //TODO: ADD DAMAGE TO ENEMIES
+            //TODO: Make it destroyable?
+            //TODO: Implement another shoot mechanic?
+            Shoot();
+        }
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        enemypos = FindObjectOfType<BaseEnemy>().transform.position;
+        toEnemy = enemypos - transform.position;
+    }
+    void Shoot()
+    {
+     
+        if(timer <= 0  && toEnemy.magnitude < sightreach)
+        {
+            ArrowController arrowToShoot = GameManager.Instance.GetArrow(transform.position).GetComponent<ArrowController>();
+            arrowToShoot.Owner = player.gameObject;
+            arrowToShoot.transform.up = toEnemy;
+
+            timer = initialtimer;
+            DecreaseLifeTime();
+        }
+
+        timer--;
+    }
+
+    void DecreaseLifeTime()
+    {
+        lifetime--;
+    }
+}
