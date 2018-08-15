1bd2f6a4d3304322130ed26a6c749be484b577be
Benedikt Kirchmeier <33699887+BeneDev@users.noreply.github.com>
2018-08-15 11:28:45 +0200

    Highscores get read from website

---
+++ Assets/Scripts/HighscoreData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HighscoreData
+{
+
+    [System.Serializable]
+    public class HighscoreDataEntry
+    {
+        public HighscoreDataEntry()
+        {
+            user = "";
+            score = 0;
+        }
+
+        public string user;
+        public int score;
+    }
+
+    public List<HighscoreDataEntry> entries;
+
+    public HighscoreData()
+    {
+        entries = new List<HighscoreDataEntry>();
+    }
+
+}
---
+++ Assets/Scripts/NetworkScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkScript : MonoBehaviour
+{
+
+    [SerializeField] string url;
+
+    [ContextMenu("Test")]
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(GetScores());
+    }
+
+    IEnumerator GetScores()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError(www.error);
+        }
+        else
+        {
+            //successful
+            Debug.Log(www.downloadHandler.text);
+
+            //parse json text
+            var data = JsonUtility.FromJson<HighscoreData>(www.downloadHandler.text);
+            for (int i = 0; i < data.entries.Count; i++)
+            {
+                Debug.Log(data.entries[i].user + ": " + data.entries[i].score);
+            }
+        }
+    }
+}
